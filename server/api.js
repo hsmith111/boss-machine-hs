1d5475a4aea23604c8afec74aea1eec29114d8d2
Heidi Smith <heidi.smith@ramseysolutions.com>
2026-03-13 08:37:43 -0500

    Refactor api router into separate file

--- server/api.js
+++ server/api.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const apiRouter = express.Router();
 
+const minionsRouter = require('./minionsAPIs');
+const ideasRouter = require('./ideasAPIs');
+const meetingsRouter = require('./meetingsAPIs');
+
+apiRouter.use('/minions', minionsRouter);
+apiRouter.use('/ideas', ideasRouter);
+apiRouter.use('/meetings', meetingsRouter);
 
 
 module.exports = apiRouter;
